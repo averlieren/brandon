b34525cae66f21478e1060c7d15baf683892d999
Brandon Nguyen <brandon.nguyen1129@gmail.com>
2020-03-18 08:51:43 -0500

    remove debug code

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,6 @@ extern crate num;
 #[path = "bvm/vm.rs"]
 pub mod bvm;
 
-use bvm::memory::Memory;
-
 fn main() {
-    let mem = Memory::new();
-    mem.write(2929, 2929);
-
-    assert_eq!(mem.read(29), None);
-    assert_ne!(mem.read(2929), None);
-    assert_eq!(mem.read(2929).unwrap(), 2929);
-
-    mem.write(2929, 2930);
-
-    assert_eq!(mem.read(2929).unwrap(), 2930);
 
 }
