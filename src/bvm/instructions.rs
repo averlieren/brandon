e594ba6f561770f72528d180c4bf9acc6c9a0b8c
Brandon Nguyen <brandon.nguyen1129@gmail.com>
2020-03-17 19:14:47 -0500

    add instructions

---
+++ src/bvm/instructions.rs
@@ -0,0 +1,60 @@
+use std::cell::RefCell;
+
+#[allow(non_camel_case_types)]
+#[derive(PartialEq, FromPrimitive)]
+pub enum Opcode {
+    MOV_REG_REG = 0,
+    MOV_REG_MEM,
+    MOV_MEM_REG,
+    MOV_MEM_MEM,
+    MOV_REG_IMM,
+    MOV_MEM_IMM,
+    SWP,
+    JMP_IMM,
+    JMP_REG,
+    JSR,
+    CMP_EQ_REG_REG,
+    CMP_LE_REG_REG,
+    CMP_GE_REG_REG,
+    CMP_LT_REG_REG,
+    CMP_GT_REG_REG,
+    CMP_EQ_REG_IMM,
+    CMP_LE_REG_IMM,
+    CMP_GE_REG_IMM,
+    CMP_LT_REG_IMM,
+    CMP_GT_REG_IMM,
+    ADD,
+    FADD,
+    SUB,
+    FSUB,
+    MUL,
+    FMUL,
+    DIV,
+    FDIV,
+    AND,
+    NOT,
+    CAL,
+    FILE_LOAD,
+    INVALID
+}
+
+pub struct Instruction {
+    opcode: Opcode,
+    bytes: RefCell<Vec<u8>>
+}
+
+impl Instruction {
+    pub fn new() -> Instruction {
+        Instruction {
+            opcode: Opcode::INVALID,
+            bytes: RefCell::new(Vec::with_capacity(0))
+        }
+    }
+
+    pub fn with_data(opcode: Opcode, bytes: Vec<u8>) -> Instruction {
+        Instruction {
+            opcode: opcode,
+            bytes: RefCell::new(bytes)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate num;
+#[macro_use] extern crate num_derive;
+
 #[path = "bvm/vm.rs"]
 pub mod bvm;
 
