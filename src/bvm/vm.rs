c5a1dfd72d951ca28be97d8e36e342c4f762f63a
Brandon Nguyen <brandon.nguyen1129@gmail.com>
2020-03-18 11:17:01 -0500

    begin vm

--- src/bvm/vm.rs
+++ src/bvm/vm.rs
@@ -9,3 +9,50 @@ pub mod memory;
 
 #[path = "registers.rs"]
 pub mod registers;
+
+use registers::Registers;
+use memory::Memory;
+
+// If there are no instrucions for this long, then halt.
+const TIMEOUT: u32 = 128;
+
+pub struct VM {
+    pub mem: Memory,
+    pub reg: Registers,
+    pub addr: u32,
+    pub running: bool
+}
+
+impl VM {
+    pub fn new() -> VM {
+        VM {
+            mem: Memory::new(),
+            reg: Registers::new(),
+            addr: 0,
+            running: false
+        }
+    }
+
+    pub fn run(&mut self) {
+        self.addr = 0;
+        self.running = true;
+        let mut nop = 0;
+
+        loop {
+            let memory = self.mem.read(0);
+
+            match memory {
+                None => nop += 1,
+                Some(_) => {
+                    nop = 0;
+                }
+            }
+
+            if nop >= TIMEOUT {
+                break;
+            }
+
+            self.addr += 1;
+        }
+    }
+}
